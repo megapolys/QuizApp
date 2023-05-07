05b950d83f4e8d793b70f353fd7e76fe530d288a
dlosev <i@dlosev.ru>
2023-05-07 12:30:03 +0300

    authorization fix

--- src/main/java/com/example/servingwebcontent/config/WebSecurityConfig.java
+++ src/main/java/com/example/servingwebcontent/config/WebSecurityConfig.java
@@ -3,7 +3,9 @@ package com.example.servingwebcontent.config;
 import com.example.servingwebcontent.UserRepository;
 
 import com.example.servingwebcontent.domain.Role;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,8 +20,11 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import javax.sql.DataSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,9 +33,6 @@ import java.util.stream.StreamSupport;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-
-    @Autowired
-    UserRepository userRepository;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -48,32 +50,27 @@ public class WebSecurityConfig {
         return http.build();
     }
 
+    @Bean(name="dataSource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName("org.postgresql.Driver")
+                .url("jdbc:postgresql://localhost:5432/postgres")
+                .username("postgres")
+                .password("admin")
+                .build();
+    }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        final List<UserDetails> users = new ArrayList<>();
-        for (com.example.servingwebcontent.domain.User user : userRepository.findAll()) {
-            String[] roles = user.getRoles().stream().map(Enum::name).toList().toArray(new String[]{});
-            UserDetails userDetails = User.builder().passwordEncoder(NoOpPasswordEncoder.getInstance()::encode)
-                    .username(user.getUsername())
-                    .password("{noop}" + user.getPassword())
-                    .roles(roles)
-                    .build();
-            users.add(userDetails);
-        }
-        return new InMemoryUserDetailsManager(users);
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
-//
-//    @Bean
-//    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-//        return http.getSharedObject(AuthenticationManagerBuilder.class)
-//                .jdbcAuthentication()
-//                .usersByUsernameQuery("select username, password, active from usr where username=?")
-//                .authoritiesByUsernameQuery("select u.username, ur.roles from usr u inner join user_role ur on u.id = ur.user_id where u.username=?")
-////                .userDetailsService(userDetailsService)
-//                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-//                .and()
-//                .build();
-//    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select username, password, active from usr where username=?");
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select u.username, ur.roles from usr u inner join user_role ur on u.id = ur.user_id where u.username=?");
+        return jdbcUserDetailsManager;
+    }
 
 }
