15fd43712e9891a524814a90cd93ff74d1c76e96
dlosev <i@dlosev.ru>
2023-05-07 12:31:27 +0300

    authorization fix

--- src/main/java/com/example/servingwebcontent/config/WebSecurityConfig.java
+++ src/main/java/com/example/servingwebcontent/config/WebSecurityConfig.java
@@ -1,34 +1,18 @@
 package com.example.servingwebcontent.config;
 
-import com.example.servingwebcontent.UserRepository;
-
-import com.example.servingwebcontent.domain.Role;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import javax.sql.DataSource;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.StreamSupport;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -62,6 +46,7 @@ public class WebSecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
+        //noinspection deprecation
         return NoOpPasswordEncoder.getInstance();
     }
 
