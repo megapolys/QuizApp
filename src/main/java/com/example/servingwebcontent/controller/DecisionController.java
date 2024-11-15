e13f0da6cf4be632abc09ec4fee3daac1df5badf
d.losev <d.losev@infosec.ru>
2024-11-15 09:00:34 +0300

    Complete decisions page

--- src/main/java/com/example/servingwebcontent/controller/DecisionController.java
+++ src/main/java/com/example/servingwebcontent/controller/DecisionController.java
@@ -1,43 +1,22 @@
 package com.example.servingwebcontent.controller;
 
-import com.example.servingwebcontent.service.decision.DecisionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
 public class DecisionController {
 
-    private final DecisionService decisionService;
-
     /**
      * Получение всех решений вместе с группами
      *
-     * @return decisions - Решения без групп
-     * groups - Группы с решениями
-     * decisionTab - Активная вкладка решений
+     * @return decisionTab - Активная вкладка решений
      */
     @GetMapping("/decisions")
     public String getDecisions(Model model) {
         model.addAttribute("decisionTab", "active");
         return "decisions/decisions";
-    }
-
-    /**
-     * Получение страницы обновления решения
-     *
-     * @param decisionId - Идентификатор решения
-     */
-    @GetMapping("/decisions/updateAction/{decisionId}")
-    public String decisionUpdateAction(
-        @PathVariable Long decisionId,
-        RedirectAttributes redirectAttributes
-    ) {
-        redirectAttributes.addFlashAttribute("changeDecision", decisionService.getDecisionById(decisionId));
-        return "redirect:/decisions";
     }
 }
